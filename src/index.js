c3bde5a8847bbc0a0458f93bf17b71155911548c
jsndz <jaisondz9360.com>
2023-09-26 21:12:53 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const {PORT} = require('./config/serverConfig');
+const ApiRoutes = require('./router/index');
+const db = require('./models/index')
+
+const setupandstart = () =>{
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}))
+    app.use('/api',ApiRoutes)
+    
+    app.listen(PORT,()=>{
+        console.log(`Server started  at ${PORT}`);
+
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+    })
+}
+setupandstart();
